3b68344fbbeca082ddd8963004165e90dfbf27f5
ManuFlycatch <manu.p@flycatchtech.com>
2021-05-19 12:03:40 +0530

    Basic Routes with express

--- Express+Typescript/server/src/index.ts
+++ Express+Typescript/server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hellooo..wassup")
+import express,{ Request, Response } from 'express';
+const app = express();
+
+app.get('/',(req: Request,res: Response) => {
+    res.send('hello..express working on typescript')
+
+})
+
+app.listen(3000,()=> {
+    console.log('listening on port 3000');
+})
