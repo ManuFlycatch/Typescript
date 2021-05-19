0b1561220d546a9502054bb39ef654228ff9590c
ManuFlycatch <manu.p@flycatchtech.com>
2021-05-19 15:07:47 +0530

    login

--- Express+Typescript/server/src/index.ts
+++ Express+Typescript/server/src/index.ts
@@ -1,11 +1,14 @@
-import express,{ Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(router)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['laskdjf'] }));
+app.use(router);
 
-app.listen(3000,()=> {
-    console.log('listening on port 3000');
-})
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
