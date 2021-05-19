1a174f464dddd2ee82bda17eea139fb539c7b917
ManuFlycatch <manu.p@flycatchtech.com>
2021-05-19 13:52:48 +0530

    Parsing from Bodies

--- Express+Typescript/server/src/index.ts
+++ Express+Typescript/server/src/index.ts
@@ -1,10 +1,10 @@
 import express,{ Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser'
 const app = express();
 
-app.get('/',(req: Request,res: Response) => {
-    res.send('hello..express working on typescript')
-
-})
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(router)
 
 app.listen(3000,()=> {
     console.log('listening on port 3000');
