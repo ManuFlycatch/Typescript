e686fafe2cdc31d5946345c453ec100fa3504de3
ManuFlycatch <manu.p@flycatchtech.com>
2021-05-20 15:36:28 +0530

    adding and testing use decorator

--- Express+Typescript/server/build/controllers/decorators/controller.js
+++ Express+Typescript/server/build/controllers/decorators/controller.js
@@ -1,8 +1,14 @@
 "use strict";
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.controller = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
+var Metadatakeys_1 = require("./Metadatakeys");
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
@@ -10,8 +16,9 @@ function controller(routePrefix) {
             var routeHandler = target.prototype[key];
             var path = Reflect.getMetadata('Metadatakeys.path', target.prototype, key);
             var method = Reflect.getMetadata('Metadatakeys.method', target.prototype, key);
+            var middlewares = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.middleware, target.prototype, key) || [];
             if (path) {
-                router[method]("" + routePrefix + path, routeHandler);
+                router[method].apply(router, __spreadArray(__spreadArray(["" + routePrefix + path], middlewares), [routeHandler]));
             }
         }
     };
--- Express+Typescript/server/src/controllers/decorators/controller.ts
+++ Express+Typescript/server/src/controllers/decorators/controller.ts
@@ -14,8 +14,13 @@ export function controller(routePrefix: string) {
       target.prototype, key);
       const method: Methods = Reflect.getMetadata('Metadatakeys.method',
        target.prototype, key)
+
+      const middlewares = Reflect.getMetadata(
+          MetadataKeys.middleware,target.prototype,key
+      ) || [];
+
       if (path) {
-        router[method](`${routePrefix}${path}`, routeHandler);
+        router[method](`${routePrefix}${path}`,...middlewares, routeHandler);
       }
     }
   };
