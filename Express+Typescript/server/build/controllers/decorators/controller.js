a24360cb5bcf5c6b25b74daee7ee67be393f5f96
ManuFlycatch <manu.p@flycatchtech.com>
2021-05-20 16:39:04 +0530

    bodyvalidator decorator

--- Express+Typescript/server/build/controllers/decorators/controller.js
+++ Express+Typescript/server/build/controllers/decorators/controller.js
@@ -9,16 +9,37 @@ exports.controller = void 0;
 require("reflect-metadata");
 var AppRouter_1 = require("../../AppRouter");
 var Metadatakeys_1 = require("./Metadatakeys");
+function bodyValidators(keys) {
+    return function (req, res, next) {
+        if (!req.body) {
+            res.status(422).send('Invalid request');
+            return;
+        }
+        for (var _i = 0, keys_1 = keys; _i < keys_1.length; _i++) {
+            var key = keys_1[_i];
+            if (!req.body[key]) {
+                res.status(422).send("Missing property " + key);
+                return;
+            }
+        }
+        next();
+    };
+}
 function controller(routePrefix) {
     return function (target) {
         var router = AppRouter_1.AppRouter.getInstance();
         for (var key in target.prototype) {
             var routeHandler = target.prototype[key];
-            var path = Reflect.getMetadata('Metadatakeys.path', target.prototype, key);
-            var method = Reflect.getMetadata('Metadatakeys.method', target.prototype, key);
-            var middlewares = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.middleware, target.prototype, key) || [];
+            var path = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.path, target.prototype, key);
+            var method = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.method, target.prototype, key);
+            var middlewares = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.middleware, target.prototype, key) ||
+                [];
+            var requiredBodyProps = Reflect.getMetadata(Metadatakeys_1.MetadataKeys.validator, target.prototype, key) ||
+                [];
+            var validator = bodyValidators(requiredBodyProps);
             if (path) {
-                router[method].apply(router, __spreadArray(__spreadArray(["" + routePrefix + path], middlewares), [routeHandler]));
+                router[method].apply(router, __spreadArray(__spreadArray(["" + routePrefix + path], middlewares), [validator,
+                    routeHandler]));
             }
         }
     };
